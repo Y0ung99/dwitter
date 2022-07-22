baf8f6d9d8abdcf68077c9b95294e15873140de9
Kum <37989299+Y0ung99@users.noreply.github.com>
2022-07-22 19:38:58 +0900

    Revert "Sequelize ORM를 data단에 적용"

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import {initSocket} from './connection/socket.js';
-import { sequelize } from './db/database.js';
+import { db } from './db/database.js';
 
 const app = express();
 
@@ -28,7 +28,6 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 })
 
-sequelize.sync().then(() => {
-    const server = app.listen(config.host.port);
-    initSocket(server);
-})
+db.getConnection();
+const server = app.listen(config.host.port);
+initSocket(server);
